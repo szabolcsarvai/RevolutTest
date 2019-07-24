38946311d8d94329e9774f4d6bb3b404f124d82f
Szabolcs Arvai <arvaiszabolcs93@gmail.com>
2019-07-24 10:17:39 +0300

    Fix item switch mechanism.

--- app/src/main/java/com/szabolcs/revoluttest/feature/main/CurrencyAdapter.kt
+++ app/src/main/java/com/szabolcs/revoluttest/feature/main/CurrencyAdapter.kt
@@ -3,50 +3,72 @@ package com.szabolcs.revoluttest.feature.main
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.LinearLayoutManager
+
 
 class CurrencyAdapter : RecyclerView.Adapter<CurrencyViewHolder>(), CurrencyValueChangeListener {
 
-    private var itemViewModels = mutableListOf<CurrencyViewModel>()
+    private lateinit var layoutManager: LinearLayoutManager
+    private var items = mutableListOf<CurrencyViewModel>()
 
     var currencyItemSelectedListener = object : CurrencyItemSelectedListener {
         override fun onItemSelected(position: Int) {
+            if (position == 0) return
             switchItems(position)
         }
     }
 
-   private fun switchItems(pos: Int) {
-        val firstItem = itemViewModels[0]
-        firstItem.currencyChangeListener = null
-        firstItem.isSelected.set(false)
-        val selectedItem = itemViewModels[pos]
-        selectedItem.currencyChangeListener = this
-        selectedItem.isSelected.set(true)
-        itemViewModels[0] = selectedItem
-        itemViewModels[pos] = firstItem
-        notifyItemChanged(0)
-        notifyItemChanged(pos)
+    private fun restoreScrollPositionAfterAdAdded() {
+        val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+
+        if (firstVisibleItemPosition == 0) {
+            layoutManager.scrollToPosition(0)
+        }
+    }
+
+    private fun switchItems(pos: Int) {
+        val item1 = items.removeAt(0)
+        item1.isSelected.set(false)
+        item1.currencyChangeListener = null
+
+        val item2 = items.removeAt(pos - 1)
+        item2.isSelected.set(true)
+        item2.currencyChangeListener = this
+
+        items.add(0, item2)
+        items.add(pos, item1)
+
+        notifyItemMoved(pos, 0)
+        notifyItemMoved(1, pos)
+
+        restoreScrollPositionAfterAdAdded()
     }
 
     fun updateItems(newItems: List<CurrencyViewModel>) {
         newItems[0].currencyChangeListener = this
 
-        val diffUtil = DiffUtil.calculateDiff(CurrencyDiffUtilCallback(itemViewModels, newItems))
+        val diffUtil = DiffUtil.calculateDiff(CurrencyDiffUtilCallback(items, newItems))
         diffUtil.dispatchUpdatesTo(this)
-        itemViewModels.clear()
-        itemViewModels.addAll(newItems)
+        items.clear()
+        items.addAll(newItems)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         CurrencyViewHolder.create(parent, currencyItemSelectedListener)
 
-    override fun getItemCount() = itemViewModels.size
+    override fun getItemCount() = items.size
 
     override fun onBindViewHolder(holder: CurrencyViewHolder, position: Int) {
-        holder.bind(itemViewModels[position])
+        holder.bind(items[position])
+    }
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        layoutManager = recyclerView.layoutManager as LinearLayoutManager
+        super.onAttachedToRecyclerView(recyclerView)
     }
 
     override fun onValueChanged(newValue: Float, selectedCurrencyRate: Float) {
-        for (itemViewModel in itemViewModels) {
+        for (itemViewModel in items) {
             itemViewModel.setSelectedCurrency(newValue, selectedCurrencyRate)
         }
     }
